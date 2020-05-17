0d185a3819e6278b74b04cc4258dbbff2c6db496
Maciej Gorczyca <maciej.dariusz.gorczyca@gmail.com>
2020-05-17 12:04:31 +0200

    Working test version

---
+++ LeagueStatus/LeagueStatus.Designer.cs
@@ -0,0 +1,170 @@
+﻿﻿namespace LeagueStatus
+{
+    partial class LeagueStatusForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources =
+                new System.ComponentModel.ComponentResourceManager(typeof(LeagueStatusForm));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize) (this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 79);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(440, 87);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(440, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Set status message";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 50);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Status:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.label2.Location = new System.Drawing.Point(252, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 50);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "not connected";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {1000, 0, 0, 0});
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 225);
+            this.numericUpDown1.Maximum = new decimal(new int[] {30000, 0, 0, 0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(213, 23);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {3000, 0, 0, 0});
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(231, 225);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(221, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Change interval";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 254);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(213, 78);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Start timer";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(231, 254);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(221, 78);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Stop timer";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 338);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(440, 91);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Website";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // LeagueStatusForm
+            // 
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(464, 441);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon) (resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MinimumSize = new System.Drawing.Size(480, 480);
+            this.Name = "LeagueStatusForm";
+            this.Text = "LeagueStatus";
+            this.Load += new System.EventHandler(this.LeagueStatusForm_Load);
+            ((System.ComponentModel.ISupportInitialize) (this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ LeagueStatus/LeagueStatus.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using Timer = System.Timers.Timer;
+
+// move into diff file
+// https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/loot.json
+
+namespace LeagueStatus
+{
+    public partial class LeagueStatusForm : Form
+    {
+
+        private LeagueConnection lc;
+        private Timer timer = new Timer();
+        private bool isInUse = false;
+
+        public LeagueStatusForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LeagueStatusForm_Load(object sender, EventArgs e)
+        {
+            lc = new LeagueConnection();
+            
+            timer.Elapsed += OnTimedEvent;
+            timer.Interval = 3000;
+            timer.Enabled = false;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (lc.IsConnected)
+            {
+                label2.Text = "connected";
+                JsonObject me = await getMe();
+                me["statusMessage"] = textBox1.Text;
+                //String statusMessage = getStatusMessage(me);
+                lc.Put("/lol-chat/v1/me", me.ToString());
+            }
+            else
+            {
+                label2.Text = "not connected";
+            }
+        }
+        
+        private async Task<dynamic> getMe() { return (JsonObject) await lc.Get("/lol-chat/v1/me"); }
+        
+        private async void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            if (lc.IsConnected && !isInUse)
+            {
+                isInUse = true;
+                JsonObject me = await getMe();
+                String currentStatus = (String) me["statusMessage"];
+                String result;
+                if (Char.IsHighSurrogate(currentStatus[0]))
+                {
+                    result = string.Join("", currentStatus.Skip(2)) + currentStatus[0] + currentStatus[1];
+                }
+                else
+                {
+                    result = string.Join("", currentStatus.Skip(1)) + currentStatus[0];
+                }
+
+                me["statusMessage"] = result;
+                lc.Put("/lol-chat/v1/me", me.ToString());
+                isInUse = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer.Interval = (int)numericUpDown1.Value; 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/MaciejGorczyca/LeagueStatus");
+        }
+    }
+}
